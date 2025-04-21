b7a27eb1cf6c95293e722570f68e56a2a3d16cf1
barathkumaran2003 <barathkumaran2003@gmail.com>
2025-04-21 23:00:03 +0530

    final change

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import Portfoliohome from './Components/Portfoliohome'
-import './Components/PortfolioAnimation.js';
-import './Components/Navigation.js';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
