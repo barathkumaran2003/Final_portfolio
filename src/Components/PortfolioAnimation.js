75fdd6a991a1804a0ab5af4ac2b6560929e0c291
barathkumaran2003 <barathkumaran2003@gmail.com>
2025-04-27 13:46:30 +0530

    final change

--- src/Components/PortfolioAnimation.js
+++ src/Components/PortfolioAnimation.js
@@ -1,12 +1,15 @@
+console.log('IntersectionObserver supported?', 'IntersectionObserver' in window);
+
 const observer = new IntersectionObserver(entries => {
   entries.forEach(entry => {
+    console.log('Observing:', entry.target);
     if (entry.isIntersecting) {
+      console.log('Visible:', entry.target);
       entry.target.classList.add('visible');
     }
   });
 });
 
-// Observe each element with different animations
 document.querySelectorAll('.in-animation4, .in-animation5, .in-animation6, .in-animation7').forEach(section => {
   observer.observe(section);
 });
