c4e8f4933e4b02ccbb531eec7d05f563599975f8
barathkumaran2003 <barathkumaran2003@gmail.com>
2025-05-01 12:12:20 +0530

    final change

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,6 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import './PortfolioAnimation.js';
-import './Navigation.js';
 function App() {
   const [count, setCount] = useState(0)
 
